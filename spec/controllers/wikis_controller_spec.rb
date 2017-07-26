d79e41203f6f297bb1b00c2786e9578600a77be8
Laura Slingerland <laura.sling@gmail.com>
2017-07-26 12:02:31 -0600

    User Story 3: Working with failing tests

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -11,12 +11,29 @@ RSpec.describe WikisController, type: :controller do
     end
 
     it "assigns [my_wiki] to @wikis" do
-       get :index
-       expect(assigns(:wikis)).to eq([my_wiki])
-     end
+      get :index
+      expect(assigns(:wikis)).to eq([my_wiki])
+    end
   end
 
-  describe "GET new" do
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_wiki.id}
+      expect(response).to render_template :show
+    end
+
+     it "assigns my_wiki to @wiki" do
+      get :show, {id: my_wiki.id}
+      expect(assigns(:wiki)).to eq(my_wiki)
+    end
+  end
+
+  describe "GET #new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
@@ -27,40 +44,76 @@ RSpec.describe WikisController, type: :controller do
       expect(response).to render_template :new
     end
 
-    it "instantiates @wiki" do
+     it "instantiates @wiki" do
       get :new
       expect(assigns(:wiki)).not_to be_nil
     end
   end
 
-  describe "WIKI create" do
+  describe "POST create" do
     it "increases the number of Wiki by 1" do
-      expect{wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Wiki,:count).by(1)
+      expect{wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}}.to change(Wiki,:count).by(1)
     end
 
     it "assigns the new wiki to @wiki" do
-      wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}
       expect(assigns(:wiki)).to eq Wiki.last
     end
 
     it "redirects to the new wiki" do
-      wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}
       expect(response).to redirect_to Wiki.last
     end
   end
-  #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
 
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, params: { id: my_wiki.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, params: { id: my_wiki.id }
+      expect(response).to render_template :edit
+    end
+  end
+
+   describe "PUT update" do
+    it "updates wiki with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+
+      updated_wiki = assigns(:wiki)
+
+      expect(updated_wiki.id).to eq my_wiki.id
+      expect(updated_wiki.title).to eq new_title
+      expect(updated_wiki.body).to eq new_body
+    end
+
+    it "redirects to the updated wiki" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+
+      expect(response).to redirect_to my_wiki
+    end
+  end
+
+   describe "DELETE destroy" do
+    it "deletes the wiki" do
+      delete :destroy, {id: my_wiki.id}
+
+      count = Wiki.where({id: my_wiki.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to wiki index" do
+      delete :destroy, {id: my_wiki.id}
+
+      expect(response).to redirect_to wikis_path
+    end
+  end
 end
