efa7fa04ea4bc0462b75fd6309d9e678ca0d2a4d
Laura Slingerland <laura.sling@gmail.com>
2017-07-25 20:19:44 -0600

    User Story 1: Mailer added

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -1,3 +1,9 @@
 class ConfirmMailer < ApplicationMailer
   default from: "laura.sling@gmail.com"
+
+  def confirm_email(current_user)
+    @current_user = current_user
+    @url  = 'https://dry-coast-33228.herokuapp.com/'
+    mail(to: current_user.email, subject: 'Welcome to Blocipedia')
+  end
 end
