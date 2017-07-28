131e49228f648135cf6b3a2cf594631d9dd27fbf
Laura Slingerland <laura.sling@gmail.com>
2017-07-28 00:13:47 -0600

    User Story 3: NOW Completed

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -5,16 +5,18 @@ RSpec.describe WikisController, type: :controller do
 
   let(:my_wiki) { Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false) }
 
-  let(:my_user) { FactoryGirl.create!(:user, email: "user@blocipedia.com", password: "password") }
+  let(:my_user) { FactoryGirl.create(:user, email: "user@blocipedia.com", password: "password") }
 
 
   describe "GET #index" do
     it "returns http success" do
+      sign_in my_user
       get :index
       expect(response).to have_http_status(:success)
     end
 
     it "assigns [my_wiki] to @wikis" do
+      sign_in my_user
       get :index
       expect(assigns(:wikis)).to eq([my_wiki])
     end
@@ -22,16 +24,19 @@ RSpec.describe WikisController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
+      sign_in my_user
       get :show, {id: my_wiki.id}
       expect(response).to have_http_status(:success)
     end
 
     it "renders the #show view" do
+      sign_in my_user
       get :show, {id: my_wiki.id}
       expect(response).to render_template :show
     end
 
      it "assigns my_wiki to @wiki" do
+       sign_in my_user
       get :show, {id: my_wiki.id}
       expect(assigns(:wiki)).to eq(my_wiki)
     end
@@ -39,16 +44,19 @@ RSpec.describe WikisController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
+      sign_in my_user
       get :new
       expect(response).to have_http_status(:success)
     end
 
     it "renders the #new view" do
+      sign_in my_user
       get :new
       expect(response).to render_template :new
     end
 
      it "instantiates @wiki" do
+      sign_in my_user
       get :new
       expect(assigns(:wiki)).not_to be_nil
     end
@@ -56,15 +64,18 @@ RSpec.describe WikisController, type: :controller do
 
   describe "POST create" do
     it "increases the number of Wiki by 1" do
+      sign_in my_user
       expect{post :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}}.to change(Wiki,:count).by(1)
     end
 
     it "assigns the new wiki to @wiki" do
+      sign_in my_user
       post :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}
       expect(assigns(:wiki)).to eq Wiki.last
     end
 
     it "redirects to the new wiki" do
+      sign_in my_user
       post :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}
       expect(response).to redirect_to Wiki.last
     end
@@ -72,11 +83,13 @@ RSpec.describe WikisController, type: :controller do
 
   describe "GET #edit" do
     it "returns http success" do
+      sign_in my_user
       get :edit, { id: my_wiki.id }
       expect(response).to have_http_status(:success)
     end
 
     it "renders the #edit view" do
+      sign_in my_user
       get :edit, { id: my_wiki.id }
       expect(response).to render_template :edit
     end
@@ -84,6 +97,7 @@ RSpec.describe WikisController, type: :controller do
 
    describe "PUT update" do
     it "updates wiki with expected attributes" do
+      sign_in my_user
       new_title = RandomData.random_sentence
       new_body = RandomData.random_paragraph
 
@@ -97,6 +111,7 @@ RSpec.describe WikisController, type: :controller do
     end
 
     it "redirects to the updated wiki" do
+      sign_in my_user
       new_title = RandomData.random_sentence
       new_body = RandomData.random_paragraph
 
@@ -108,6 +123,7 @@ RSpec.describe WikisController, type: :controller do
 
    describe "DELETE destroy" do
     it "deletes the wiki" do
+      sign_in my_user
       delete :destroy, {id: my_wiki.id}
 
       count = Wiki.where({id: my_wiki.id}).size
@@ -115,8 +131,8 @@ RSpec.describe WikisController, type: :controller do
     end
 
     it "redirects to wiki index" do
+      sign_in my_user
       delete :destroy, {id: my_wiki.id}
-
       expect(response).to redirect_to wikis_path
     end
   end
