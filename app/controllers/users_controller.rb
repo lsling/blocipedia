cd64ccc9c1153039e4abea9c6ad0426a325668e9
Laura Slingerland <laura.sling@gmail.com>
2017-08-14 21:32:37 -0600

    User Stories 7, 8, 9: All Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,9 @@
 class UsersController < ApplicationController
 
   def downgrade
-    current_user.update_attributes(role: "standard")
-    current_user.wikis.where(private: true).update_all(private: false)
+    current_user.update(role: "standard")
+    current_user.wikis.update_all(private: false)
     flash[:notice] = "You have been cancelled, #{current_user.email}."
     redirect_to edit_user_registration_path(current_user)
   end
-
-  # def downgrade
-  #   current_user.standard!
-  #   private_wikis = current_user.wikis.where(private: true)
-  #   private_wikis.each do |wiki|
-  #     wiki.update(private: false)
-  #   current_user.wikis.where(private: true).update_all(private: false)
-  #   end
-  #   flash[:notice] = "You have been cancelled, #{current_user.email}."
-  #   redirect_to edit_user_registration_path(current_user)
-  # end
 end
